aee8a747bcb2ef8f79c2a4b8f65aa761b1b9fe04
Cameron Whipple <clwhipple@protonmail.com>
2024-07-17 21:37:23 -0500

    Added let's encrypt support and vaultwarden

--- scripts/bootstrap-ansible-host.sh
+++ scripts/bootstrap-ansible-host.sh
@@ -1,29 +1,17 @@
 #!/bin/bash
 
-function install_software(){
-
-    # Install Ansible
+function install_ansible(){
     sudo apt update
     sudo apt install software-properties-common unzip
     sudo add-apt-repository --yes --update ppa:ansible/ansible
     sudo apt -y install ansible sshpass
-
-    # Install Bitwarden CLI
-    unzip bw-linux-2024.3.1.zip
-    sudo mv bw /usr/bin
-    sudo chown root:root /usr/bin/bw
-    sudo chmod 755 /usr/bin/bw
-
 }
 
-function install_root(){
-    config_dir="/home/$(whoami)/.config"
-    mkdir -p $config_dir
-    cp pki-root.crt $config_dir/pki-root.crt
-    chmod 700 $config_dir/pki-root.crt
-    echo "export NODE_EXTRA_CA_CERTS=\"$config_dir/pki-root.crt\"" >> ~/.bashrc
+function install_software(){
+    sudo apt install vim
+    sudo snap install bw
+    sudo snap install bitwarden
 }
 
-#install_software
-install_root
-
+install_ansible
+install_software
