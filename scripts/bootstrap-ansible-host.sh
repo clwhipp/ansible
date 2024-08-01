24810bd4c41cf059ccc8b04cd7621235460ae9ed
Cameron Whipple <clwhipple@pm.me>
2024-07-31 23:16:56 -0500

    Migrate to docker-services repository from administrative

--- scripts/bootstrap-ansible-host.sh
+++ scripts/bootstrap-ansible-host.sh
@@ -2,7 +2,7 @@
 
 function install_ansible(){
     sudo apt update
-    sudo apt install software-properties-common unzip
+    sudo apt -y install software-properties-common unzip
     sudo add-apt-repository --yes --update ppa:ansible/ansible
     sudo apt -y install ansible sshpass
 }
